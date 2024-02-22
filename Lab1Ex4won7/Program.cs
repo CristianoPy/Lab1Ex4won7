f7d200da6f36d696ffac6bb21ef1e0047c71920c
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-02-22 10:47:08 +0200

    Add project files.

---
+++ Lab1Ex4won7/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Scrieti un program care va afisa semnul unui numar citit de la tastatura
+• Daca numarul este pozitiv, va afisa “+”
+• Daca numarul este negativ, va afisa “-”
+• Daca numarul este 0, va afisa “0”*/
+
+int num;
+Console.WriteLine("Introduce un numar: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0)
+{
+    Console.WriteLine("+");
+}
+
+else if (num < 0)
+{
+    Console.WriteLine("-");
+}
+
+else if (num == 0)
+{
+    Console.WriteLine("0");
+}
+
+   Console.ReadLine();
